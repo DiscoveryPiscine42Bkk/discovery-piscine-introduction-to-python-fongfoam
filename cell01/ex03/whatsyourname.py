d06dce1851bea4f26fb379f3c7d69b739efc90c0
fongfoam <foamctmbj@gmail.com>
2025-06-17 04:52:50 +0000

    add whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input ("Hey, what's your first name? : Waranya")
+last_name = input ("And your last name? : Yupaking")
+print ("Well, pleased to meet you, Waranya Yupaking", first_name, last_name)
